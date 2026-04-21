a4bdae6abf981e77bbacc0651d84772248069c28
irishmaiden18 <laurapohl18@gmail.com>
2026-04-21 14:54:02 -0400

    fixes misunderstanding for exercise 1

--- src/components/exercise1/TotalDisplay.jsx
+++ src/components/exercise1/TotalDisplay.jsx
@@ -3,11 +3,15 @@ import React, { useEffect, useState } from "react";
 export default function TotalDisplay({bill, tip}) {
   // Total is hardcoded / static; doesn’t know about BillInput or TipPercentageInput
 
-  const [total, setTotal] = useState (0.00)
+  // const [total, setTotal] = useState (0.00)
 
-  useEffect(() => {
-    setTotal(Number(bill) + Number(tip))
-  }, [bill, tip])
+  // useEffect(() => {
+  //   setTotal(Number(bill) + (Number(bill) * (Number(tip) * .01)))
+  // }, [bill, tip])
+
+  // total doesn't need to be a state variable
+  // tip should be a percentage of the bill not flat number
+  const total = Number(bill) + (Number(bill) * (Number(tip) * .01))
   
   return (
     <div>
